66e54a2517e9776d49c7c9c1560ded269f229d71
Edgar Lopez <elopezanaya@gmail.com>
2023-12-30 15:12:51 -0800

    a bit better

--- app/src/main/java/com/omni/omniwidgetapp/MainActivity.kt
+++ app/src/main/java/com/omni/omniwidgetapp/MainActivity.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.webkit.ConsoleMessage
 import android.webkit.WebChromeClient
 import android.webkit.WebView
-import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 
@@ -22,7 +21,6 @@ class MainActivity : AppCompatActivity() {
 
         val launchButton: ImageButton = findViewById<ImageButton>(R.id.buttonLoadUrl)
         editTextUrl = findViewById(R.id.editTextUrl)
-        editTextData = findViewById(R.id.editTextScript)
 
         launchButton.setOnClickListener {
             val wb = findViewById<WebView>(R.id.webviewLCW)
@@ -41,22 +39,38 @@ class MainActivity : AppCompatActivity() {
             wb.settings.allowFileAccess= true
 
             val url = editTextUrl.text.toString()
-           // val script = editTextData.text.toString()
 
             print("ELOPEZ : URL :$url");
 
             if ( url.isNotEmpty()){
                 wb.loadUrl(editTextUrl.text.toString())
             }
+        }
 
-            /*else{
 
+        val launchScriptButton: ImageButton = findViewById<ImageButton>(R.id.buttonLoadScript)
+        editTextData = findViewById(R.id.editTextScript)
+
+        launchScriptButton.setOnClickListener {
+            val wb = findViewById<WebView>(R.id.webviewLCW)
+            wb.webChromeClient = object : WebChromeClient() {
+                override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+                    Log.d("WebView", "Console: ${consoleMessage?.message()}")
+                    return true
+                }
+            }
+
+            wb.settings.databaseEnabled= true
+            wb.settings.javaScriptEnabled = true
+            wb.settings.javaScriptCanOpenWindowsAutomatically= true
+            wb.settings.domStorageEnabled= true
+            wb.settings.allowContentAccess= true
+            wb.settings.allowFileAccess= true
+
+            val script = editTextData.text.toString()
                 if (script.isNotEmpty()){
                     wb.loadData(script,  "text/html", "UTF-8")
                 }
-
-            }*/
-
         }
     }
 }
