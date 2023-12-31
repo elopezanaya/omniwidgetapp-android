fbc5819bc153fe691be906f969ee2e079c889ebc
Edgar Lopez <elopezanaya@gmail.com>
2023-12-30 18:07:53 -0800

    a bit better

--- app/src/main/java/com/omni/omniwidgetapp/MainActivity.kt
+++ app/src/main/java/com/omni/omniwidgetapp/MainActivity.kt
@@ -14,63 +14,57 @@ class MainActivity : AppCompatActivity() {
     private lateinit var editTextUrl: EditText
     private lateinit var editTextData: EditText
 
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initUrlFunctionality()
+        initScriptFunctionality()
+    }
+
+    private fun initUrlFunctionality() {
         val launchButton: ImageButton = findViewById<ImageButton>(R.id.buttonLoadUrl)
         editTextUrl = findViewById(R.id.editTextUrl)
 
         launchButton.setOnClickListener {
             val wb = findViewById<WebView>(R.id.webviewLCW)
-            wb.webChromeClient = object : WebChromeClient() {
-                override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
-                    Log.d("WebView", "Console: ${consoleMessage?.message()}")
-                    return true
-                }
-            }
-
-            wb.settings.databaseEnabled= true
-            wb.settings.javaScriptEnabled = true
-            wb.settings.javaScriptCanOpenWindowsAutomatically= true
-            wb.settings.domStorageEnabled= true
-            wb.settings.allowContentAccess= true
-            wb.settings.allowFileAccess= true
-
+            setViewSettings(wb)
             val url = editTextUrl.text.toString()
-
-            print("ELOPEZ : URL :$url");
-
-            if ( url.isNotEmpty()){
+            if (url.isNotEmpty()) {
                 wb.loadUrl(editTextUrl.text.toString())
             }
         }
+    }
 
-
+    private fun initScriptFunctionality() {
         val launchScriptButton: ImageButton = findViewById<ImageButton>(R.id.buttonLoadScript)
         editTextData = findViewById(R.id.editTextScript)
 
         launchScriptButton.setOnClickListener {
             val wb = findViewById<WebView>(R.id.webviewLCW)
-            wb.webChromeClient = object : WebChromeClient() {
-                override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
-                    Log.d("WebView", "Console: ${consoleMessage?.message()}")
-                    return true
-                }
-            }
-
-            wb.settings.databaseEnabled= true
-            wb.settings.javaScriptEnabled = true
-            wb.settings.javaScriptCanOpenWindowsAutomatically= true
-            wb.settings.domStorageEnabled= true
-            wb.settings.allowContentAccess= true
-            wb.settings.allowFileAccess= true
+            setViewSettings(wb)
 
             val script = editTextData.text.toString()
-                if (script.isNotEmpty()){
-                    wb.loadData(script,  "text/html", "UTF-8")
-                }
+            if (script.isNotEmpty()) {
+                wb.loadData(script, "text/html", "UTF-8")
+            }
         }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setViewSettings(wb: WebView) {
+        wb.webChromeClient = object : WebChromeClient() {
+            override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+                Log.d("WebView", "Console: ${consoleMessage?.message()}")
+                return true
+            }
+        }
+
+        wb.settings.databaseEnabled = true
+        wb.settings.javaScriptEnabled = true
+        wb.settings.javaScriptCanOpenWindowsAutomatically = true
+        wb.settings.domStorageEnabled = true
+        wb.settings.allowContentAccess = true
+        wb.settings.allowFileAccess = true
     }
 }
